45a555693c91c535c73e67c70c4eafc0338f3bf1
matthias.kallenbach@googlemail.com <matthias.kallenbach@gmail.com>
2019-12-17 10:25:26 +0100

    css+js lint, clean up

--- config/production.js
+++ config/production.js
@@ -1,36 +1,52 @@
 const
     fs = require('fs-extra'),
-    ConfigClass = require('./config.js');
+    ConfigClass = require('./config.js'),
+    spawn = require('child_process').spawn,
+    Crypto = require('crypto');
 
 module.exports = class extends ConfigClass {
     constructor() {
         super();
 
+        this.salt = 'rambazamba';
+        this.hash = Crypto.createHash('md5').update(this.salt).digest("hex");
+
         this.config = {
             mode: 'production',
-            entry: [
-                './src/app.js',
-                './src/scss/index.scss'
-            ],
-            output: {
-                filename: 'js/bundle.js',
-                path: `${this.appPath}/dist/prod`,
+            entry: {
+                app: './src/app.js',
             },
-
+            output: {
+                filename: './js/[name].js',
+                path: `${this.appPath}/dist/prod/`,
+            },
 
             module: {
                 rules: [
                     {
                         test: /\.html?$/,
                         loader: "template-literals-loader"
-                    },{
+                    },
+                    {
+                        test: /\.(png|svg|jpg|gif|jpe?g)$/,
+                        use: [
+                            {
+                                options: {
+                                    name: "[name].[ext]",
+                                    outputPath: "../images/"
+                                },
+                                loader: "file-loader"
+                            }
+                        ]
+                    },
+                    {
                         test: /.scss$/,
                         use: [
                             'style-loader',
                             {
                                 loader: 'file-loader',
                                 options: {
-                                    name: 'bundle.css',
+                                    name: '[name].css',
                                     outputPath: '../../dist/prod/css/'
                                 }
                             },
@@ -56,16 +72,13 @@ module.exports = class extends ConfigClass {
                 {
                     apply: (compiler) => {
                         compiler.hooks.afterEmit.tap('Complete', (compilation) => {
-                            console.log('>>> HOOKED');
-                            fs.removeSync(`${this.appPath}/docs`);
-                            fs.mkdirSync(`${this.appPath}/docs`);
-                            fs.mkdirSync(`${this.appPath}/docs/js`);
-                            fs.mkdirSync(`${this.appPath}/docs/css`);
+                            fs.copySync(`${this.appPath}/public/`, `${this.appPath}/dist/prod`);
+                            fs.copySync(`${this.appPath}/dist/prod`, `${this.appPath}/docs`);
 
-                            fs.copyFileSync(`${this.appPath}/dist/prod/js/bundle.js`, `${this.appPath}/docs/js/bundle.js`);
-                            fs.copyFileSync(`${this.appPath}/dist/prod/css/bundle.css`, `${this.appPath}/docs/css/bundle.css`);
-                            fs.copyFileSync(`${this.appPath}/public/index.html`, `${this.appPath}/docs/index.html`);
-                            fs.copyFileSync(`${this.appPath}/public/index.html`, `${this.appPath}/dist/prod/index.html`);
+                            sedReplace('/css', '/drone-boat-dashboard/css', `${this.appPath}/docs/css/app.css`);
+                            //sedReplace('/images', '/drone-boat-dashboard/images', `${this.appPath}/docs/css/app.css`);
+                            sedReplace('?hash', `?${this.hash}`, `${this.appPath}/docs/index.html`);
+                            sedReplace('debug: true', 'debug: false', `${this.appPath}/docs/index.html`);
                         });
                     }
                 }
@@ -73,4 +86,25 @@ module.exports = class extends ConfigClass {
         };
         return this.mergeConfig();
     };
+};
+
+const sedReplace = (replaceFrom, replaceTo, replaceFile) => {
+    const replaceCommand = `s#${replaceFrom}#${replaceTo}#g`;
+    const spawnOptions = [
+        '-i',
+        replaceCommand,
+        replaceFile
+    ];
+    setTimeout(() => {
+        const proc = spawn('sed', spawnOptions);
+        proc.on('error', (err) => {
+            console.error('>>> ERROR', err);
+        });
+        proc.stdout.on('data', (data) => {
+            console.log(data.toString());
+        });
+        proc.stderr.on('data', (data) => {
+            console.log(data.toString());
+        });
+    }, 2000);
 };
